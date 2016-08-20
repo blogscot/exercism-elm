1857c5026d5f1baf5cf4e64943b8bb9e8a2175ee
blogscot <iain.diamond@gmail.com>
2016-08-20 15:57:25 +0100

    Refactor Raindrops

--- raindrops/tests/Raindrops.elm
+++ raindrops/tests/Raindrops.elm
@@ -3,31 +3,11 @@ module Raindrops exposing (..)
 import String
 
 raindrops : Int -> String
-raindrops num =
-  raindrops' num ""
-
-raindrops' : Int -> String -> String
-raindrops' num acc =
+raindrops x =
   let
-    rem_three = num `rem` 3
-    rem_five = num `rem` 5
-    rem_seven = num `rem` 7
+    drops = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
   in
-  if num == 0 then acc
-    else if rem_three == 0 then raindrops' (shake num 3) (acc ++ "Pling")
-    else if rem_five == 0 then raindrops' (shake num 5) (acc ++ "Plang")
-    else if rem_seven == 0 then raindrops' (shake num 7) (acc ++ "Plong")
-    else if String.isEmpty acc then toString num
-      else acc
-
-
--- Reduces number until it contains no more factors
--- ie.
-shake : Int -> Int -> Int
-shake num factor =
-  let
-    remainder = num `rem` factor
-  in
-  if num == 1 then 0
-    else if remainder == 0 then shake (num // factor) factor
-    else num
+    List.foldr (\(a, b) acc ->
+        if x % a == 0 then b :: acc else acc) [] drops
+    |> (\xs ->
+      if List.isEmpty xs then toString x else String.concat xs)
