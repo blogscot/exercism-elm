f75ee2d8cbf56842087cba01b9a006ea6f897dca
blogscot <iain.diamond@gmail.com>
2016-08-22 19:34:50 +0100

    Strain

---
+++ strain/tests/Strain.elm
@@ -0,0 +1,8 @@
+module Strain exposing (..)
+
+keep : (a -> Bool) -> List a -> List a
+keep f =
+  List.foldr (\x y -> if f x then x::y else y) []
+
+discard : (a -> Bool) -> List a -> List a
+discard f = keep (f >> not)
