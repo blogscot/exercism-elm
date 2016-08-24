089df0a9a43ff9773dff64a41759185438848525
blogscot <iain.diamond@gmail.com>
2016-08-24 12:30:36 +0100

    Refactor hamming

--- hamming/tests/Hamming.elm
+++ hamming/tests/Hamming.elm
@@ -4,14 +4,8 @@ import String exposing (length, toList)
 
 distance : String -> String -> Maybe Int
 distance strand1 strand2 =
-  let
-    valid = length strand1 == length strand2
-    s1 = strand1 |> toList
-    s2 = strand2 |> toList
-  in
-  if valid then
-    List.map2 (,) s1 s2
-    |> List.map (\(x, y) -> if x /= y then 1 else 0)
-    |> List.sum >> Just
+  if length strand1 == length strand2 then
+    List.map2 (\x y -> x /= y) (toList strand1) (toList strand2)
+    |> List.filter identity >> List.length >> Just
   else
     Nothing
