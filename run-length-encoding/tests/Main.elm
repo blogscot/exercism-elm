9392da7ed397460c6d4071e4a6aa7661d3af25e5
blogscot <iain.diamond@gmail.com>
2016-08-26 18:51:20 +0100

    Run Length Encoding

--- run-length-encoding/tests/Main.elm
+++ run-length-encoding/tests/Main.elm
@@ -4,14 +4,7 @@ import Test.Runner.Node exposing (run)
 import Json.Encode exposing (Value)
 import Test exposing (..)
 import Expect
-import RunLengthEncoding exposing (
-  version,
-  decode,
-  encode,
-  take_while,
-  group_by_char,
-  compress,
-  decompress)
+import RunLengthEncoding exposing (version, decode, encode, take_while, group_by_char)
 import String exposing (toList)
 
 
@@ -25,10 +18,6 @@ tests =
             \() -> Expect.equal ['A', 'A'] (take_while (\x -> x == 'A') (toList "AABBBCCCC") )
           , test "group by char" <|
             \() -> Expect.equal [['A', 'A', 'A'], ['B'], ['C']] (group_by_char ['A', 'A', 'A', 'B', 'C'])
-          , test "decompress string" <|
-            \() -> Expect.equal (String.toList "12W1B12W3B24W1B") (decompress <| String.toList "12WB12W3B24WB")
-          , test "decompress string" <|
-            \() -> Expect.equal (String.toList "12WB12W3B24WB") (compress <| String.toList "12W1B12W3B24W1B")
           , test "encode simple" <|
             \() -> Expect.equal "2A3B4C" (encode "AABBBCCCC")
           , test "decode simple" <|
@@ -49,10 +38,10 @@ tests =
             \() ->
                 Expect.equal "WWWWWWWWWW"
                     (decode "10W")
-        -- , test "encode unicode" <|
-        --     \() -> Expect.equal "⏰3⚽2⭐⏰" (encode "⏰⚽⚽⚽⭐⭐⏰")
-        -- , test "decode unicode" <|
-        --     \() -> Expect.equal "⏰⚽⚽⚽⭐⭐⏰" (decode "⏰3⚽2⭐⏰")
+        , test "encode unicode" <|
+            \() -> Expect.equal "⏰3⚽2⭐⏰" (encode "⏰⚽⚽⚽⭐⭐⏰")
+        , test "decode unicode" <|
+            \() -> Expect.equal "⏰⚽⚽⚽⭐⭐⏰" (decode "⏰3⚽2⭐⏰")
         ]
 
 
--- run-length-encoding/tests/RunLengthEncoding.elm
+++ run-length-encoding/tests/RunLengthEncoding.elm
@@ -3,7 +3,6 @@ module RunLengthEncoding exposing (..)
 import List exposing (append, drop, head, length)
 import Regex exposing (regex)
 import String
-import Char exposing (isDigit)
 
 encode : String -> String
 encode string =
@@ -16,8 +15,8 @@ encode string =
         Maybe.withDefault ' ' (head x)
         |> String.fromChar
     in
-      acc ++ (x |> length |> toString) ++ firstChar) ""
-  |> compress'
+      acc ++ (if length x == 1
+        then "" else x |> length |> toString) ++ firstChar) ""
 
 
 group_by_char : List Char -> List (List Char)
@@ -42,53 +41,22 @@ take_while fn chars  =
 decode : String -> String
 decode string =
   string
-  |> decompress'
   |> Regex.split Regex.All (regex "(\\d+)(\\D)")
-  |> decode3
+  |> List.filter (\x -> x /= "")
+  |> decode'
 
 
-decode3 : List String -> String
-decode3 list =
+decode' : List String -> String
+decode' list =
   case list of
+    x :: y :: rest ->
+      case String.toInt x of
+        Ok num ->
+          String.repeat num y ++ decode' rest
+        Err _ -> x ++ decode' (y :: rest)
+    x :: rest -> x ++ decode' rest
     [] -> ""
 
-    "" :: digit :: letter :: rest ->
-      let
-        num = Result.withDefault 0 (String.toInt digit)
-      in
-        String.repeat num letter ++ decode3 rest
-
-    letter :: rest -> letter ++ decode3 rest
-
-
---  "12W1B12W3B24W1B" => "12WB12W3B24WB"
-compress : List Char -> List Char
-compress list =
-  case list of
-    x::y::z::rest ->
-      if not (isDigit x) && y == '1' && not (isDigit z)
-        then x :: compress (z :: rest)
-      else x :: compress (y :: z :: rest)
-    x::y::[] -> if x == '1' then [y] else list
-    x :: [] -> [x]
-    [] -> []
-
-compress' : String -> String
-compress' = String.toList >> compress >> String.fromList
-
---  "12WB12W3B24WB" => "12W1B12W3B24W1B"
-decompress : List Char -> List Char
-decompress list =
-  case list of
-    x::y::rest ->
-      if not (isDigit x) && not (isDigit y)
-        then x :: '1' :: decompress (y :: rest)
-      else x :: decompress (y :: rest)
-    x :: [] -> [x]
-    [] -> []
-
-decompress' : String -> String
-decompress' = String.toList >> decompress >> String.fromList
 
 version : Int
 version = 2
